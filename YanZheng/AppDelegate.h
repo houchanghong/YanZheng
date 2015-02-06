3d8ea7f21467e62f91ab84b0196b84476d6893b2
houchanghong <houchanghong@me.com>
2015-02-06 09:07:17 +0800

    Initial Commit

---
+++ YanZheng/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YanZheng
+//
+//  Created by Mac on 15/2/6.
+//  Copyright (c) 2015年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YanZheng/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YanZheng
+//
+//  Created by Mac on 15/2/6.
+//  Copyright (c) 2015年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
